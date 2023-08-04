218cdc4aac88c177d5a4fbd318e29745ccdfbe09
lucasaragao29 <95982348+lucasaragao29@users.noreply.github.com>
2023-08-04 10:59:32 -0300

    Add files via upload

---
+++ app/data/QuizData.js
@@ -0,0 +1,118 @@
+export default data = [
+    {
+        pergunta: "De acordo com o CONAMA é considerado impacto ambiental qualquer alteração física, química ou biológica que afetam:",
+        opcoes: ["A saúde e o bem estar geral","Os resultados econômicos","A diversidade cultural","O meio-ambiente"],
+        respostaCorreta: "A saúde e o bem estar geral"
+    },
+    {
+        pergunta: "Usar o solo de forma inapropriada gera muitos problemas ambientais. Para reduzir o impacto ambiental é preciso desenvolver:",
+        opcoes: ["Alimentos Trangênicos","Agricultura familiar","Agricultura Convencional","Agricutura Economica"],
+        respostaCorreta: "Agricultura familiar"
+    },
+    {
+        pergunta:"Umas das metas da ODS 1 é acabar com a pobreza em todas as suas dimensões.Qual alternativa NÃO é uma ação de combate á pobreza:",
+        opcoes:["Inclusão Social","Politicas Sociais","Distruibuição de Sopão","Dar Esmolas"],
+        respostaCorreta:"Politicas Sociais"
+    },
+    {
+        pergunta:"Para proteger e restaurar o ecossistema relacionado a água é preciso entender que a poluição por eutrofização pode causar:",
+        opcoes:["Surgimento de novas especies","Aumento de plantas aquáticas","Mortandade de peixes","Inserção de novas especieis"],
+        respostaCorreta:"Mortandade de peixes"
+    },
+    {
+        pergunta:"Acabar com todas as formas de discrinação contra a mulheres e meninas é uma ação para alcançar a igualde de genêro, que contruibui para:",
+        opcoes:["Ascenção Social","Empoderamento feminino","Violência contra a mulher","Maior indice de gravidez"],
+        respostaCorreta:"Empoderamento feminino"
+    },
+    {
+        pergunta:"Em 1997, aconteceu no Brasil a I Confêrencia Nacional de Educação Ambiental, que definiu a Educação Ambiental como:",
+        opcoes:["Essencial para a educação formal","Paralela à educaçao formal","Desnessária a educaçao escolar","Algo irrelevante para o futuro"],
+        respostaCorreta:"Essencial para a educação formal"
+    },
+    {
+        pergunta:"Para reduzir a intensificação do efeito estufa e os impactos ambientais é necessário que as indústrias adotem:",
+        opcoes:["Medida de segurança","Capacitação de pessoal","Produção sustentável","Ter menos ar-condionados"],
+        respostaCorreta:"Produção sustentável"
+    },
+    {
+        pergunta:"O turismo sustentável contruibui para o crescimento econômico e o trabalho descente, pois gerencia as atividades econômicas, sociais e ambientais sem desprezar:",
+        opcoes:["O foco ecônomico","A integridade cultural","O transporte local","O pagamento de taxas"],
+        respostaCorreta:"A integridade cultural"
+    },
+    {
+        pergunta:"A energia não renovável é um tipo de energia que polui o ambiente, contribui para a chuva ácida e o aquecimento global. É um tipo NÃO renovável:",
+        opcoes:["Eólica","Hidrelétrica","Solar","Energia Nuclear"],
+        respostaCorreta:"Energia Nuclear"
+    },
+    {
+        pergunta:"Os impactos ambientais podem ser positivos ou negativos. É considerado impacto ambiental positivo:",
+        opcoes:["Reduzir a biodiversidade","Promover o bem estar de todos","Incentivar a mutação genética","Clonar Animais"],
+        respostaCorreta:"Promover o bem estar de todos"
+    },
+    {
+        pergunta:"O uso do solo de forma inapropriada gera muitos problemas ambientais. Que tipo de sistemas tem por objetivo unir a agricultura e a conservação ambiental:",
+        opcoes:["Agroflorestas","Agricultura familiar","Permacultura","Uso de produtos quimicos"],
+        respostaCorreta:"Agroflorestas"
+    },
+    {
+        pergunta:"Os ODS representam um plano de açao global para eliminar a pobreza extrema e a fome. Contribui para a erradição da pobreza:",
+        opcoes:["Garantir a equidade social","Estimular a competição econômica","Reduzir os programas sociais","Fornecer comida para desabrigados"],
+        respostaCorreta:"Garantir a equidade social"
+    },
+    {
+        pergunta:"Em relação à água potável e saneamento, pode-se dizer que o processo causado pelo excesso de dejetos orgânicos despejados nos cursos hídricos é:",
+        opcoes:["Eutrofização","Decomposição","Fosferação","Limpeza"],
+        respostaCorreta:"Eutrofização"
+    },
+    {
+        pergunta:"Uma das formas de garantir a igualdade de gênero é dar ás mulheres, direitos iguais aos recursos econômicos, através de:",
+        opcoes:["Igualdade de oportunidade e salário","Distinguir as atividades de gênero","Apoiar a discriminação","Não dar apoio"],
+        respostaCorreta:"Distinguir as atividades de gênero"
+    },
+    {
+        pergunta:"De acordo com a Política Nacional de Educação Ambiental (PNEA), a Educação ambiental é:",
+        opcoes:["Obrigaçao de Estado","Especifica do técnico ambiental","Não é importante","Um direito de todos"],
+        respostaCorreta:"Um direito de todos"
+    },
+    {
+        pergunta:"Para promover a industruialização sustentável e formentar a inovação. É preciso:",
+        opcoes:["Aumentar a taxa de emprego","Fortalecer a pesquisa cientifica","Investir em maquinário","Formentar a mão de obra"],
+        respostaCorreta:"Fortalecer a pesquisa cientifica"
+    },
+    {
+        pergunta:"Constitui meta do ODS 8, elaborar e implementar políticas para promover o turismo sustentável. É objetivo principal do turismo sustentável:",
+        opcoes:["Gerir os recursos naturais","Desequilíbrio social","Promover a cidade","Aumentar taxas"],
+        respostaCorreta:"Desequilíbrio social"
+    },
+    {
+        pergunta:"Umas das formas mais eficiente de compensar a neutralizão de caborno é por meio de energias renováveis.É considerado energia limpa",
+        opcoes:["Carvão","Energia Solar","Gás Natural","Nuclear"],
+        respostaCorreta:"Energia Solar",
+    },
+    {
+        pergunta:"Uma das formas de contruibuir para tornar as cidades comunidades sustentavéis é por meio de:",
+        opcoes:["Reaproveitamento Hidríco","Novas Hidréletricas","Biopirataria","Criação de Lixões"],
+        respostaCorreta:"Reaproveitamento Hidríco"
+    },
+    {
+        pergunta:"Para reduzir as desigualdades é preciso promover a inclusão social,econômica e política de todo. Pode-se alcançar a equidade social através de:",
+        opcoes:["Educação tradicional","Educação para o trabalho","Educação de qualidade para todos","Segregar o ensino"],
+        respostaCorreta:"Educação de qualidade para todos",
+    },
+    {
+        pergunta:"O aumento das queimadas e do desmatamento da Amazônia pode provocar a diminuição da evapotranspiração da floresta. Uma das consequências da evopotranspiração é:",
+        opcoes:["Desequilibrio hídrico","Desmatamento","Aumento das queimadas","Extinção da fauna"],
+        respostaCorreta:"Desequilivrio hídrico"
+    },
+    {
+        pergunta:"O aumento do consumo de água, a poluição das reservas hídricas, a degradação dos recursos naturais e as mudanças climáticas podem contruibuir para:",
+        opcoes:["Efeito estufa","Chuva Acida","Escassez hídrica","Excesso de água"],
+        respostaCorreta:"Escassez hídrica"
+    },
+    {
+        pergunta:"Além de reduzir a emissão de CO2, outra forma de contruibuir para combater o aquecimento global é:",
+        opcoes:["Reduzir o consumo de peixe","Ultilizar energia renovável","Uso de etanol","Usar ar-condicionado"],
+        respostaCorreta:"Uso de etanol"
+    }
+
+]
